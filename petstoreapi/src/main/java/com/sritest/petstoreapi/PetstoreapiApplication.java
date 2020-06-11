1b4612c848fc2a521fc41e6a278d6192a79b213e
Srikar Kini <srikar.kini@mastercard.com>
2020-06-11 15:03:12 +0100

    added missing files

---
+++ petstoreapi/src/main/java/com/sritest/petstoreapi/PetstoreapiApplication.java
@@ -0,0 +1,13 @@
+package com.sritest.petstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoreapiApplication.class, args);
+	}
+
+}
